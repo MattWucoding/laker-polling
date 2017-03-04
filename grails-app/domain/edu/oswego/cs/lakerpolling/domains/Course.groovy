1a4bc3ea889a3ca8434a07ce099a69330e183c35
jpost27 <jpost94@gmail.com>
2017-03-04 17:21:43 -0500

    Update Course.groovy

--- grails-app/domain/edu/oswego/cs/lakerpolling/domains/Course.groovy
+++ grails-app/domain/edu/oswego/cs/lakerpolling/domains/Course.groovy
@@ -3,12 +3,16 @@ package edu.oswego.cs.lakerpolling.domains
 
 class Course {
     String name
-    String CRN
+    String crn
+
+    static hasMany = [user: User]
 
     static mapping = {
+        id column: 'crn'
     }
 
     static constraints = {
         name nullable: true
+        crn nullable: false
     }
 }
