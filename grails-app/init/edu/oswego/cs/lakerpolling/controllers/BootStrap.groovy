7d11abd9edfa81060bd36bfbb3f1e0719909ee5a
jpost27 <jpost94@gmail.com>
2017-03-07 09:16:06 -0500

    Update BootStrap.groovy

--- grails-app/init/edu/oswego/cs/lakerpolling/controllers/BootStrap.groovy
+++ grails-app/init/edu/oswego/cs/lakerpolling/controllers/BootStrap.groovy
@@ -1,27 +1,22 @@
 package edu.oswego.cs.lakerpolling.controllers
 
 class BootStrap {
-
     def init = { servletContext ->
         // 5 Example Users
         new User(firstName: "Josh", lastName: "Post", email: "jpost@oswego.edu",
                 imageUrl: "http://cdn.business2community.com/wp-content/uploads/2016/03/Vd3MJo.jpg",
-                role: RoleType.ADMIN,authToken: null).save()
+                role: RoleType.ADMIN,authToken: null).save(flush:true)
         new User(firstName: "Keith", lastName: "Martin", email: "kmartin5@oswego.edu",
-                imageUrl: null,role: RoleType.INSTRUCTOR,authToken: null).save()
+                imageUrl: null,role: RoleType.INSTRUCTOR,authToken: null).save(flush:true)
         new User(firstName: "Akeem", lastName: "Davis", email: "adavis20@oswego.edu",
-                imageUrl: null,role: RoleType.INSTRUCTOR,authToken: null).save()
+                imageUrl: null,role: RoleType.INSTRUCTOR,authToken: null).save(flush:true)
         new User(firstName: "Ricky", lastName: "Rojas", email: "rrojas@oswego.edu",
-                imageUrl: null, role: null,authToken: null,course: Course.findAllByName("csc480")).save()
+                imageUrl: null, role: null,authToken: null,course: Course.findAllByName("csc480")).save(flush:true)
         new User(firstName: "Matt", lastName: "Wu", email: "jwu5@oswego.edu",
-                imageUrl: null,role: null,authToken: null,course: Course.findAllByName("csc480")).save()
-
-        //Example Semester
-        new Semester(year: 2017, season: Season.SPRING).save()
+                imageUrl: null,role: null,authToken: null,course: Course.findAllByName("csc480")).save(flush:true)
 
         //Example Course
-        new Course(name: csc480, crn: 12345, users: User.findAllByRole(null), semester: Semester.findBySeasonAndYear(Season.SPRING,2017)).save()
-
+        new Course(name: csc480, crn: 12345, users: User.findAllByRole(null), semester: Semester.findBySeasonAndYear(Season.SPRING,2017)).save(flush:true)
 
     }
     def destroy = {
