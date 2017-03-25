7885f9f449c13e75496c0e8e8b67041e971500a0
MattWucoding <jwu5@oswego.edu>
2017-03-24 20:24:31 -0400

    Update Role.groovy
    Add enum INSTRUCTOR, ADMIN, STUDENT for RoleType

--- grails-app/domain/edu/oswego/cs/lakerpolling/domains/Role.groovy
+++ grails-app/domain/edu/oswego/cs/lakerpolling/domains/Role.groovy
@@ -3,8 +3,14 @@ package edu.oswego.cs.lakerpolling.domains
 import edu.oswego.cs.lakerpolling.util.RoleType
 
 class Role {
+    enum RoleType {
+        INSTRUCTOR, ADMIN, STUDENT
+    }
+
     RoleType type
 
     static belongsTo = [user: User]
-    static constraints = {}
+    static constraints = {
+        type blank: false
+    }
 }
